a663d73a65f2a930d32dce630b8794207db20c9a
ChrisMitov <mitov.christian@gmail.com>
2016-03-21 20:38:04 +0200

    Add Maven Project

---
+++ src-main/com/finance/tracker/exception/FinanceTrackerException.java
@@ -0,0 +1,26 @@
+package com.finance.tracker.exception;
+
+public class FinanceTrackerException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8064345019175231244L;
+
+	public FinanceTrackerException() {
+		super();
+	}
+
+	public FinanceTrackerException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public FinanceTrackerException(String message) {
+		super(message);
+	}
+
+	public FinanceTrackerException(Throwable cause) {
+		super(cause);
+	}
+
+}
---
+++ src-main/com/finance/tracker/model/User.java
@@ -0,0 +1,5 @@
+package com.finance.tracker.model;
+
+public class User {
+
+}
