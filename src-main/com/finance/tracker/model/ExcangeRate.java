fb67a6331b95b92e2846f105fe082d2ec80c97a9
Petya Kapchina <petya.kapchina@abv.bg>
2016-03-22 22:47:52 +0200

    adding classes

---
+++ src-main/com/finance/tracker/model/ExcangeRate.java
@@ -0,0 +1,84 @@
+package com.finance.tracker.model;
+
+public class ExcangeRate {
+	private static double bgnToUds = 1.74441;
+	private static double udsToBgn = 0.57326;
+	private static double eurToBgn = 1.95;
+	private static double eurToUds = 1.12;
+	private static double udsToEur = 0.89;
+	private static double bgnToEur = 0.51;
+
+
+	public double convertMoney(double amount, Currancy cuurentCurrancy, Currancy newCurrancy) {
+		if (amount > 0) {
+			if (cuurentCurrancy.equals(Currancy.BGN) && newCurrancy.equals(Currancy.EUR)) {
+				return converetFromBGNToEUR(amount);
+			}
+			if (cuurentCurrancy.equals(Currancy.BGN) && newCurrancy.equals(Currancy.UDS)) {
+				return converetFromBGNToUDS(amount);
+			}
+			if (cuurentCurrancy.equals(Currancy.EUR) && newCurrancy.equals(Currancy.BGN)) {
+				return converetFromEURToBGN(amount);
+			}
+			if (cuurentCurrancy.equals(Currancy.EUR) && newCurrancy.equals(Currancy.UDS)) {
+				return converetFromEURToUDS(amount);
+			}
+			if (cuurentCurrancy.equals(Currancy.UDS) && newCurrancy.equals(Currancy.BGN)) {
+				return converetFromUDSTOBGN(amount);
+			}
+			if (cuurentCurrancy.equals(Currancy.UDS) && newCurrancy.equals(Currancy.EUR)) {
+				return converetFromUDSToEUR(amount);
+			}
+		}
+		return 0;
+	}
+
+	private double converetFromEURToUDS(double eur) {
+		if (eur > 0)
+			return eur * eurToUds;
+		else {
+			return 0;
+		}
+	}
+
+	private double converetFromEURToBGN(double eur) {
+		if (eur > 0)
+			return eur * eurToBgn;
+		else {
+			return 0;
+		}
+	}
+
+	private double converetFromBGNToUDS(double bgn) {
+		if (bgn > 0)
+			return bgn * bgnToUds;
+		else {
+			return 0;
+		}
+	}
+
+	private double converetFromBGNToEUR(double bgn) {
+		if (bgn > 0)
+			return bgn * bgnToEur;
+		else {
+			return 0;
+		}
+	}
+
+	private double converetFromUDSTOBGN(double uds) {
+		if (uds > 0)
+			return uds * udsToBgn;
+		else {
+			return 0;
+		}
+	}
+
+	private double converetFromUDSToEUR(double uds) {
+		if (uds > 0)
+			return uds * udsToEur;
+		else {
+			return 0;
+		}
+	}
+
+}
