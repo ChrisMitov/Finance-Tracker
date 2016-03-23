7b511e1b4f4197a434a877f378e0ae547afb24b7
Petya Kapchina <petya.kapchina@abv.bg>
2016-03-23 18:19:18 +0200

    new exception

---
+++ src-main/com/finance/tracker/exception/PasswordException.java
@@ -0,0 +1,29 @@
+package com.finance.tracker.exception;
+
+public class PasswordException extends Exception {
+
+	public PasswordException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
